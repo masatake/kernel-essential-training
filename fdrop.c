a89c92e0189d5ff4ccca64375778239ee56d0634
Masatake YAMATO <yamato@redhat.com>
2020-09-13 02:55:58 +0900

    fdrop: new program

---
+++ fdrop.c
@@ -0,0 +1,26 @@
+#include <fcntl.h>
+#include <stdio.h>
+
+int
+main(int argc, char **argv)
+{
+	int fd;
+
+	if (argc < 1) {
+		fprintf (stderr, "missing file\n");
+		return 1;
+	}
+
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0) {
+		perror ("open");
+		return 1;
+	}
+
+	if (posix_fadvise(fd, 0, 0, POSIX_FADV_DONTNEED) < 0) {
+		perror ("fadvise");
+		return 1;
+	}
+
+	return 0;
+}
