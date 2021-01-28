e54ffb3555619aa986dbb5fd29c7a93f9cd4c659
Masatake YAMATO <yamato@redhat.com>
2021-01-28 22:17:52 +0900

    add shell scripts for building strace on my branch
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

---
+++ build-strace/rhel8.sh
@@ -0,0 +1,27 @@
+set -xe
+
+sudo dnf install \
+     --enablerepo=rhel-8-for-x86_64-baseos-rpms \
+     --enablerepo=rhel-8-for-x86_64-appstream-rpms \
+     autoconf automake git
+
+sudo dnf \
+     --enablerepo=rhel-8-for-x86_64-baseos-rpms \
+     --enablerepo=rhel-8-for-x86_64-appstream-rpms \
+     --enablerepo=codeready-builder-for-rhel-8-x86_64-rpms \
+     --enablerepo=rhel-8-for-x86_64-baseos-source-rpms \
+     --enablerepo=rhel-8-for-x86_64-appstream-source-rpms \
+     --enablerepo=codeready-builder-for-rhel-8-x86_64-source-rpms \
+     builddep strace
+
+if ! [ -e rhel8-strace-kvm ]; then
+    git clone --branch=kvm-snapshot --single-branch http://github.com/masatake/strace rhel8-strace-kvm
+fi
+(
+    cd rhel8-strace-kvm
+    bash ./bootstrap
+    ./configure
+    make
+    test -x ./strace
+    cp strace ../strace-rhel8
+)
