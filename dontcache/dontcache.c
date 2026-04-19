bb31ad9feb78bf188bc58ae28701035367282860
Masatake YAMATO <yamato@redhat.com>
2026-04-20 05:43:07 +0900

    dontcache: add "create" and "write" actions
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

--- dontcache/dontcache.c
+++ dontcache/dontcache.c
@@ -26,6 +26,23 @@ static void do_read (int fd, int io_flags)
     }
 }
 
+static void do_write (int fd, int io_flags)
+{
+    char buffer[4096];
+    struct iovec iov;
+
+    iov.iov_base = buffer;
+    iov.iov_len = sizeof(buffer);
+
+    for (int i = 0; i < 1024 * 1024 * 1024 / sizeof(buffer); i++) {
+        int r = pwritev2 (fd, &iov, 1, -1, io_flags);
+        if (r < 0)
+            err(1, "failed in write");
+        else if (r != sizeof(buffer))
+            errx(1, "failed in write");
+    }
+}
+
 struct action {
     char *key;
     char *helpmsg;
@@ -33,9 +50,24 @@ struct action {
     int open_flags;
     int io_flags;
 } action [] = {
-    {"read+cache",      "read (with cache)",       do_read,  O_RDONLY,          0},
-    {"read+dontcache",  "read with RWF_DONTCACHE", do_read,  O_RDONLY,          RWF_DONTCACHE},
-    {"read+direct",     "read directly",           do_read,  O_RDONLY|O_DIRECT, 0},
+    {"read+cache",       "read (with cache)",
+     do_read,  O_RDONLY,                         0},
+    {"read+dontcache",   "read with RWF_DONTCACHE",
+     do_read,  O_RDONLY,                         RWF_DONTCACHE},
+    {"read+direct",      "read directly",
+     do_read,  O_RDONLY|O_DIRECT,                0},
+    {"create+cache",     "create a file with 1G data (to cache)",
+     do_write, O_WRONLY|O_CREAT|O_EXCL,          0},
+    {"create+dontcache", "create a file with 1G data (RWF_DONTCACHE)",
+     do_write,  O_WRONLY|O_CREAT|O_EXCL,         RWF_DONTCACHE},
+    {"create+diect",     "create a file with 1G data (directly)",
+     do_write, O_WRONLY|O_CREAT|O_EXCL|O_DIRECT, 0},
+    {"write+cache",      "write 1G data to the exiting file (to cache)",
+     do_write, O_WRONLY,                         0},
+    {"write+dontcache",  "write 1G data to the exiting file with RWF_DONTCACHE",
+     do_write, O_WRONLY,                         RWF_DONTCACHE},
+    {"write+direct",     "write 1G data to the exiting file directly",
+     do_write, O_WRONLY|O_DIRECT,                0},
     {NULL}
 };
 
@@ -74,7 +106,8 @@ int main(int argc, char ** argv)
     if (a == NULL)
         print_usage (argv[0], 1, stderr);
 
-    int fd = open (argv[2], a->open_flags);
+    mode_t mode = (a->open_flags & O_CREAT) ? (S_IRUSR|S_IWUSR) : 0;
+    int fd = open (argv[2], a->open_flags, mode);
     if (fd < 0)
         err(1, "failed in open %s", argv[2]);
 
